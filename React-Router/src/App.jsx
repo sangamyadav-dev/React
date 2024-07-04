426075661a2044e9182b92156de93b5f64c9ec91
Sangam Yadav <sangamyadav95k@gmail.com>
2024-07-04 19:46:07 +0530

    Done

--- React-Router/src/App.jsx
+++ React-Router/src/App.jsx
@@ -1,12 +1,18 @@
+import React from 'react'
 import './App.css'
-import {sangam}  from "./components/sangam"
+import {Header} from './components/Header/Header'
+import {Footer} from './components/Footer/Footer'
+import {Home} from './components/Home/Home'
+
 
 function App() {
   return (
     <>   
-    {sangam()}
+      <h1 className= " w-10 h-9 bg-red-800">React Router</h1>
+      {Header()}
+      {Home()}
+      {Footer()}
     </>
   )
 }
-
 export default App
